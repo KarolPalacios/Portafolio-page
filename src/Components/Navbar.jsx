39dc788e692c3fb01a9cc7dc1edff614b7f3c9dd
Karol Palacios <dayanapalaciosnieva@gmail.com>
2022-12-10 11:11:02 -0500

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,17 +6,16 @@ import Navbar from 'react-bootstrap/Navbar';
 const NavBar = () => {
     return (
         <div>
-            <Navbar>
+            <Navbar bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="#inicio">DN</Navbar.Brand>
-                    <Nav>
+                    <Nav className="me-auto">
                         <Nav.Link href="#inicio">Inicio</Nav.Link>
                         <Nav.Link href="#habilidades">Habilidades</Nav.Link>
                         <Nav.Link href="#portafolio">Portafolio</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
-
         </div>
     );
 };

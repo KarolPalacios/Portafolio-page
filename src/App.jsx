090bc3ed5173067e9871a6f3575990b43a37c3e3
Karol Palacios <dayanapalaciosnieva@gmail.com>
2022-12-11 16:07:58 -0500

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Projects from './Components/Projects'
 import Container from 'react-bootstrap/Container';
 import Skills from './Components/Skills'
 import ContactMe from './Components/ContactMe'
-import NavBar from './Components/NavBar';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 
 function App() {
@@ -12,15 +12,24 @@ function App() {
 
   return (
     <div className="App" >
-      <NavBar/>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#inicio">DN</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#inicio">Inicio</Nav.Link>
+            <Nav.Link href="#habilidades">Habilidades</Nav.Link>
+            <Nav.Link href="#portafolio">Portafolio</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
       <Container className="my-5">
         <main className="main">
-              <Home/>
-              <Skills/>
-              <Projects/>
+          <Home />
+          <Skills />
+          <Projects />
         </main>
       </Container>
-      <ContactMe/>
+      <ContactMe />
     </div>
   )
 }

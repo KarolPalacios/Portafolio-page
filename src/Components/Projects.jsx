625bc2d9e8840b3cbc8220d5b4e923d2dcf08d72
Karol Palacios <dayanapalaciosnieva@gmail.com>
2023-01-12 15:44:08 -0500

    Actu

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
 import rickymorty from '../assets/rickymorty.jpeg';
 import pokedex from '../assets/pokedex.jpeg';
 import weatherapp from '../assets/weatherapp.jpeg';
@@ -10,42 +9,52 @@ const Projects = () => {
 
     const pokeDex = () => location.href = "https://fluffy-malabi-531236.netlify.app/"
 
-    const weatherApp = () => location.href = "https://dainty-arithmetic-a4c415.netlify.app/"    
+    const weatherApp = () => location.href = "https://dainty-arithmetic-a4c415.netlify.app/"
 
     return (
         <div className="projects" id="portafolio">
             <h1>Proyectos</h1>
+
             <div className="projects-container">
-                <Card onClick={rickYMorty} >
-                    <Card.Img variant="top" src={rickymorty} />
-                    <Card.Body>
-                        <Card.Title>Rick & Morty</Card.Title>
-                        <Card.Text>
-                            Busca todos los mundos de Rick & Morty por su ID.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
 
-                <Card onClick={pokeDex} >
-                    <Card.Img variant="top" src={pokedex} />
-                    <Card.Body>
-                        <Card.Title>Pokedex</Card.Title>
-                        <Card.Text>
-                            Consulta la informacion de tu pokemon favorito
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                <div className="p-card" onClick={rickYMorty}>
+                    <div className="p-card-inner">
+                        <div className="p-card-front">
+                            <img src={rickymorty} alt="" />
+                        </div>
+                        <div className="p-card-back">
+                            <h2>Rick & Morty</h2>
+                            <p>Busca todos los mundos de Rick & Morty por su ID.</p>
+                        </div>
+                    </div>
+                </div>
 
-                <Card onClick={weatherApp} >
-                    <Card.Img variant="top" src={weatherapp} />
-                    <Card.Body>
-                        <Card.Title>Weather App</Card.Title>
-                        <Card.Text>
-                            Activa tu ubicación para saber cómo está el clima en cualquier lugar.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                <div className="p-card" onClick={pokeDex}>
+                    <div className="p-card-inner">
+                        <div className="p-card-front">
+                            <img src={pokedex} alt="" />
+                        </div>
+                        <div className="p-card-back">
+                            <h2>Pokedex</h2>
+                            <p>Consulta la informacion de tu pokemon favorito.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="p-card" onClick={weatherApp}>
+                    <div className="p-card-inner">
+                        <div className="p-card-front">
+                            <img src={weatherapp} alt="" />
+                        </div>
+                        <div className="p-card-back">
+                            <h2>Weather App</h2>
+                            <p>Activa tu ubicación para saber cómo está el clima en cualquier lugar.</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
+
         </div>
     );
 };

153053c3e87185c7c2cdada92c16f2f012e511db
Karol Palacios <dayanapalaciosnieva@gmail.com>
2023-02-08 17:42:45 -0500

    send emails

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import emailjs from '@emailjs/browser';
 
 const ContactMe = () => {
 
@@ -7,19 +8,32 @@ const ContactMe = () => {
 
     const linkedinProfile = () => location.href = "https://www.linkedin.com/in/karol-dayana-palacios-nieva-b06ba9252"
 
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_dscirmt', 'template_5zpniwb', form.current, 'a9cISZdXIntVlrzAv')
+        .then((result) => {
+            console.log(result.text);
+            alert("mensaje enviado");
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
 
     return (
         <div className="contact" id="contacto">
             <h2>Contacto</h2>
-            <Form>
+            <Form ref={form} onSubmit={sendEmail}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Email" />
+                    <Form.Control type="email" placeholder="Email" name="email"/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicText">
                     <Form.Label>Mensaje</Form.Label>
-                    <Form.Control as="textarea" placeholder="Mensaje" />
+                    <Form.Control as="textarea" placeholder="Mensaje" name="mensaje" />
                 </Form.Group>
 
                 <Button variant="outline-light" type="submit">
